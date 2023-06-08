4f2c1c835685b1e3baa1eeacc927ded8aa7f6ef2
Thanhnguyen0710 <thanhnc2000@vnpt.vn>
2023-06-09 06:13:29 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   title = '3DProjectTest';
   // modelUrl = "../assets/InteriorTest.obj";
   // modelUrl = "../assets/gate.gltf";
-  modelUrl = "../assets/HTC_Vive_Headset.gltf";
+  modelUrl = "../assets/";
   modelGltf: any;
   scene: any;
   listLayer: any = [
@@ -43,14 +43,23 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.scene = new THREE.Scene();
     this.scene.background = new THREE.Color(0xffffff);
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
     camera.position.z = 5;
     const renderer = new THREE.WebGLRenderer({ canvas: this.canvas?.nativeElement });
+    renderer.setPixelRatio( window.devicePixelRatio );
+		renderer.setSize( window.innerWidth * 0.75, window.innerHeight * 0.75); // required
+		renderer.toneMapping = THREE.ACESFilmicToneMapping;
+		renderer.toneMappingExposure = 1;
     const controls = new OrbitControls(camera, this.canvas?.nativeElement);
+    // controls.addEventListener( 'change', this.render ); // use if there is no animation loop
+		controls.minDistance = 2;
+		controls.maxDistance = 10;
+		controls.target.set( 0, 0, - 0.2 );
+		controls.update();
     // controls.addEventListener('changes', );
 
-    const loader = new GLTFLoader();
-    loader.load(this.modelUrl, (gltf) => {
+    const loader = new GLTFLoader().setPath( this.modelUrl );
+    loader.load("HTC_Vive_Headset.gltf", async (gltf) => {
       this.modelGltf = gltf.scene;
       this.scene.add(gltf.scene);
       camera.position.z = 5;
@@ -61,33 +70,33 @@ export class AppComponent implements OnInit {
     const hlight = new THREE.AmbientLight (0x404040,100);
     this.scene.add(hlight);
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff,1);
-    directionalLight.position.set(0,1,0);
-    directionalLight.castShadow = true;
-    this.scene.add(directionalLight);
+    // const directionalLight = new THREE.DirectionalLight(0xffffff,1);
+    // directionalLight.position.set(0,1,0);
+    // directionalLight.castShadow = true;
+    // this.scene.add(directionalLight);
 
-    const ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
-		this.scene.add( ambientLight );
+    // const ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+		// this.scene.add( ambientLight );
 
-		const pointLight = new THREE.PointLight( 0xffffff, 0.8 );
-		camera.add( pointLight );
-		this.scene.add( camera );
+		// const pointLight = new THREE.PointLight( 0xffffff, 0.8 );
+		// camera.add( pointLight );
+		// this.scene.add( camera );
 
-    const light = new THREE.PointLight(0xc4c4c4,10);
-    light.position.set(0,300,500);
-    this.scene.add(light);
+    // const light = new THREE.PointLight(0xc4c4c4,10);
+    // light.position.set(0,300,500);
+    // this.scene.add(light);
 
-    const light2 = new THREE.PointLight(0xc4c4c4,10);
-    light2.position.set(500,100,0);
-    this.scene.add(light2);
+    // const light2 = new THREE.PointLight(0xc4c4c4,10);
+    // light2.position.set(500,100,0);
+    // this.scene.add(light2);
 
-    const light3 = new THREE.PointLight(0xc4c4c4,10);
-    light3.position.set(0,100,-500);
-    this.scene.add(light3);
+    // const light3 = new THREE.PointLight(0xc4c4c4,10);
+    // light3.position.set(0,100,-500);
+    // this.scene.add(light3);
 
-    const light4 = new THREE.PointLight(0xc4c4c4,10);
-    light4.position.set(-500,300,500);
-    this.scene.add(light4);
+    // const light4 = new THREE.PointLight(0xc4c4c4,10);
+    // light4.position.set(-500,300,500);
+    // this.scene.add(light4);
 
     const animate = () => {
       requestAnimationFrame(animate);
@@ -96,7 +105,8 @@ export class AppComponent implements OnInit {
     animate();
   }
 
-  onChangeColor() {
+  onChangeColor(colorChange: any) {
+    this.color = colorChange;
     if (this.layerSelect) {
       this.scene.traverse((child: any) => {
         if (child instanceof THREE.Mesh) {
